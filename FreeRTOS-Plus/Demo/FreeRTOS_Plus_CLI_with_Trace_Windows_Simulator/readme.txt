d74767e7718492b1911b3809e4cc61007cfdd683
wanghao <809291634@qq.com>
2025-04-25 21:20:47 +0800

    first commit

---
+++ FreeRTOS-Plus/Demo/FreeRTOS_Plus_CLI_with_Trace_Windows_Simulator/readme.txt
@@ -0,0 +1,8 @@
+Directories:
+
++ FreeRTOS-Plus/Demo_Projects_Using_FreeRTOS_Simulator/FreeRTOS_Plus_CLI_with_Trace
+  contains a FreeRTOS windows simulator demo project for both FreeRTOS+CLI and
+  FreeRTOS+Trace.  See https://www.freertos.org/Documentation/03-Libraries/02-FreeRTOS-plus/FreeRTOS-Plus/FreeRTOS_Plus_Trace/RTOS_Trace_Instructions
+  for information on using the project.
+
+
---
+++ FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/docs/group__trace__event__apis.js
@@ -0,0 +1,25 @@
+var group__trace__event__apis =
+[
+    [ "xTraceEventAdd16", "group__trace__event__apis.html#ga117fda02be14d7344c32d80a16a16776", null ],
+    [ "xTraceEventAdd32", "group__trace__event__apis.html#ga12fc28f1cd94b5167e0c1c541e2bc123", null ],
+    [ "xTraceEventAdd8", "group__trace__event__apis.html#ga848f7f80ed87213c0c1d9f74bed0dba5", null ],
+    [ "xTraceEventAddString", "group__trace__event__apis.html#gacd2cdde9951fe313cec11003a4fe203f", null ],
+    [ "xTraceEventAddUnsignedBaseType", "group__trace__event__apis.html#ga441b595058d45b79be4fe6e7ea4f1507", null ],
+    [ "xTraceEventBegin", "group__trace__event__apis.html#gac506caaa70d385e64cd0e4fa2ad80a04", null ],
+    [ "xTraceEventEnd", "group__trace__event__apis.html#ga3e4d4ace59b52e5c2e80d34b40329917", null ],
+    [ "xTraceEventGetPayload", "group__trace__event__apis.html#gae0399574efecfbcdb1d92aa176c597f4", null ],
+    [ "xTraceEventGetRawData", "group__trace__event__apis.html#ga55e1fd2058f3507101ac46238cfe350d", null ],
+    [ "xTraceEventGetSize", "group__trace__event__apis.html#ga3d104209d01e4e1ac2fce7a1c7ebfa69", null ],
+    [ "xTraceEventPayloadRemaining", "group__trace__event__apis.html#gac2fe59add1d55f013879ee7d88503f25", null ],
+    [ "xTraceEventPayloadSize", "group__trace__event__apis.html#gaf49b771de784ae6be526dcd1b851d77e", null ],
+    [ "xTraceEventPayloadUsed", "group__trace__event__apis.html#ga7664fb00364a4a1165dc23074ef698bc", null ],
+    [ "xTraceEventAddData", "group__trace__event__apis.html#ga112f4b8cbcbe510c9d07db9b90dc620c", null ],
+    [ "xTraceEventCreate0", "group__trace__event__apis.html#ga63f12da033faf0f4de4ef2755d73b308", null ],
+    [ "xTraceEventCreate1", "group__trace__event__apis.html#ga676dd9f79556a3343072b14fac51af1d", null ],
+    [ "xTraceEventCreate2", "group__trace__event__apis.html#ga5fcbd638f23ebbe7d9a4447d0338beca", null ],
+    [ "xTraceEventCreate3", "group__trace__event__apis.html#gad64822d442e36ad8b008462994442e11", null ],
+    [ "xTraceEventCreate4", "group__trace__event__apis.html#gaadf63a13e25292a1c96330dae5e520b6", null ],
+    [ "xTraceEventCreate5", "group__trace__event__apis.html#gaebde2bb91d01e7cc4e0c1adac58d46c4", null ],
+    [ "xTraceEventCreate6", "group__trace__event__apis.html#ga7c11b915f0ac18234c3bd2dd7be8610a", null ],
+    [ "xTraceEventGetSize", "group__trace__event__apis.html#gaf8b45ef0660e9b133d74112710b2f8e9", null ]
+];
